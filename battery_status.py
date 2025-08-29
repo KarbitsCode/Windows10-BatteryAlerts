c7ad39a08884890f981d6dff6e2fca221496f57b
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2025-08-29 16:21:40 +0800

    Add keyboard combination for exiting

--- battery_status.py
+++ battery_status.py
@@ -1,6 +1,7 @@
 from psutil import sensors_battery
 from win10toast import ToastNotifier
 from time import sleep
+import keyboard
 import sys
 
 toast = ToastNotifier()
@@ -8,12 +9,18 @@ charging_toast = 0
 uncharged_toast = 0
 in80charged_toast = 0
 fullycharged_toast = 0
+running = 1
 
-while True:
+def flip():
+    global running
+    running = not running
+
+keyboard.add_hotkey('ctrl+shift+alt+o', flip)
+
+while running:
     battery = sensors_battery()
     plugged = battery.power_plugged
     percent = str(battery.percent)
-    # print(battery)
     if plugged and percent == '80' and charging_toast == 1 and in80charged_toast == 0:
         charging_toast = 0
         uncharged_toast = 0
