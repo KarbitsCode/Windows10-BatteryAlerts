d385cb64625b1c4564b19923a85dda2023e18297
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-02-27 20:24:23 +0800

    Using venv for missing modules

--- make.bat
+++ make.bat
@@ -1,2 +1,7 @@
+py -m venv .venv
+call .venv\Scripts\activate.bat
+py -m pip install -U pip
+pip install -U -r requirements.txt
 pyinstaller --clean --noconfirm --noupx battery_status.pyw
 pyinstaller --clean --noconfirm --onefile battery_status.pyw
+call .venv\Scripts\deactivate.bat
